5dbe27d209de607fdf1957b0fbab4d5deeff0f2d
Gabriel <gabrieldiascatarin26@gmail.com>
2022-05-20 14:18:23 -0300

    myLinks-1 Base do Projeto

---
+++ README.md
@@ -0,0 +1,16 @@
+## 💻 _myLinks_
+
+Ferramenta para organizar meus links em um só lugar.
+
+## 💻  Projeto desenvolvido
+<a href="https://gabrieldiasdev.github.io/myLinks/" target="_blank">Acesse o projeto</a>
+
+<img src="assets/img/readme_img.png" />
+
+## 👨‍💻 Autor
+ 
+
+Gabriel Dias Catarin, desenvolvedor front-end | Bauru, São Paulo
+
+[![Linkedin](https://i.stack.imgur.com/gVE0j.png) LinkedIn](https://www.linkedin.com/in/gabriel-dias-260857207/)
+&nbsp;
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const song = document.querySelector('#back_song');
+const play_text = document.querySelector('#play_text');
+const play_button = document.querySelector('#play_button')
+
+play_button.addEventListener('click', () => {
+    if(song.paused){
+        song.play();
+        song.volume = 0.1;
+        play_button.src = "./assets/img/pause_icon.svg";
+        play_text.innerText = "Click to pause the song";
+    }else{
+        song.pause();
+        play_button.src = "./assets/img/play_icon.svg";
+        play_text.innerText = "Click to play the song";
+    }
+});
